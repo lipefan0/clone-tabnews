b52382ed0682db182f1f4157259997bf7cd8f4a9
Felipe Oliveira <lipefan0@gmail.com>
2025-03-10 14:18:59 +0000

    feat: cria arquivo de  e  ao projeto

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,11 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as SUM;");
+  console.log(result.rows);
   response.status(200).json({
-    status: 'Alunos do curso.dev são pessoas acima da média!'
-  })
+    status: "Alunos do curso.dev são pessoas acima da média!",
+  });
 }
 
-export default status
+export default status;
